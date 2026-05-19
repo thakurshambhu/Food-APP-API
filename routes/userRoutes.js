96a50e3447a94b0486ae65fb3a9c0bf5dea4813a
thakurshambhu <shambhuthakur15@gmail.com>
2026-05-19 13:22:45 +0530

    second commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -20,4 +20,5 @@ router.post("/resetPassword", authMiddleware, resetPasswordController);
 router.post("/updatePassword", authMiddleware, updatePasswordController);
 // delete user
 router.delete("/deleteUser/:id", authMiddleware, deleteProfileController);
+// delete user
 module.exports = router;
